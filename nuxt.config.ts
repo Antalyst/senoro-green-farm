d6ba1425c396b7a3fad93879b681d92372593cc1
unknown <delux385@gmail.com>
2026-06-04 09:47:13 +0800

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
     public: {
       supabaseUrl: process.env.SUPABASE_URL || '',
       supabaseKey: process.env.SUPABASE_KEY || '',
-      apiBaseUrl: process.env.API_BASE_URL || 'https://senoro-green-farm.vercel.app',
+      apiBaseUrl: process.env.API_BASE_URL || 'https://nfwm.vercel.app',
     },
   },
 
